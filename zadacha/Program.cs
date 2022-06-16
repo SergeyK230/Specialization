72497df01cf45a771b61cf3ff7e6304869ceb3fa
sergey.k <k.sergey2010@yandex.ru>
2022-06-16 12:24:51 +0700

    create FindCountSample

--- zadacha/Program.cs
+++ zadacha/Program.cs
@@ -6,5 +6,23 @@
     }
 }
 
+int FindCountSample(string[] array)
+{
+    int countSample = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) countSample ++;
+    }
+    return countSample;
+}
+
+string[] ArraySample(string[] array)
+{
+    FindCountSample(array);
+}
+
 int count = int.Parse(Console.ReadLine());
-string [] array = new string[count];
+string[] array1 = new string[count];
+FillArray(array1);
+
+string[] array2 = new string[];
