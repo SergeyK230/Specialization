ab4be22f5a171fa986a39b3eff3fefbff3f2f4b4
sergey.k <k.sergey2010@yandex.ru>
2022-06-16 12:36:21 +0700

    create ArraySample

--- zadacha/Program.cs
+++ zadacha/Program.cs
@@ -16,13 +16,27 @@ int FindCountSample(string[] array)
     return countSample;
 }
 
-string[] ArraySample(string[] array)
+string[] ArraySample(string[] array1)
 {
-    FindCountSample(array);
+    
+    string[] array2 = new string[FindCountSample(array1)];
+    for (int i = 0; i < array2.Length; i++)
+    {
+        int j = 0;
+        for (; j < array1.Length; j++)
+        {
+            if (array1[j].Length <= 3)
+            {
+                array2[i] = array1[j];
+                break;
+            }
+        }
+    }
+    return(array2);
 }
 
 int count = int.Parse(Console.ReadLine());
 string[] array1 = new string[count];
 FillArray(array1);
 
-string[] array2 = new string[];
+string[] array2 = ArraySample(array1);
